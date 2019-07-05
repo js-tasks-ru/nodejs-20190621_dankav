3be42682ad00684e176cfd429d21dfb10368949b
Danil Kavaliou <dkavaliou.by@gmail.com>
2019-07-05 18:06:30 +0300

    4m2t - final solution

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -22,36 +22,66 @@ server.on('request', (req, res) => {
         if( path.dirname(pathname) != '.') {
           res.statusCode = 400;
           res.end('Path with dirs are not supported');
-          break;
+          return;
         }
         
-        let fileData;
+        const fileOut = fs.createWriteStream(filepath, { flags: 'wx'});
+        const limitStream = new LimitSizeStream({limit: 1048576, encoding: 'utf-8'});
 
-        const fileOut = fs.createWriteStream(filepath);
 
-        req.pipe(new LimitSizeStream({limit: 1e6, encoding: 'utf-8'}))
+        req
+          .on('aborted', () => {
+            fs.unlink(filepath);
+            res.statusCode = 501;
+            res.end('Connection was lost');
+          })
+          .pipe(limitStream)
           .on('error', (err) =>{
             if (err && err.code == 'LIMIT_EXCEEDED'){
               res.statusCode = 413;
               res.end('File is too big'); 
+              fs.unlink(filepath);
             }
-            if (err && err.code != 'ENOENT') {
-              throw err;
-            } 
           })
           .pipe(fileOut)
           .on('error', (err) => {
-            if(err && err.code != 'EEXIST') {
+            if(err && err.code == 'EEXIST' ) {
               res.statusCode = 409;
               res.end('File is already exist');
-            } else {
-              fs.unlink(filepath, (err) => {
-                if(err && err.code != 'ENOENT') {
-                  throw err;
-                } 
-              })
-            }
+            } 
           })
+          .on('close', () =>{
+            res.statusCode = 201;
+            res.end('File created');
+          });
+        
+        // req.pipe(limitStream).pipe(fileOut);
+        // req.on('aborted', () => {
+        //     fs.unlink(filepath);
+        //     res.statusCode = 501;
+        //     res.end('Connection was lost');
+        //   });
+
+        // limitStream.on('error', (err) =>{
+        //   if (err && err.code == 'LIMIT_EXCEEDED'){
+        //     res.statusCode = 413;
+        //     res.end('File is too big'); 
+        //     fs.unlink(filepath);
+        //   }
+        // });
+
+        // fileOut.on('error', (err) => {
+        //   if(err && err.code == 'EEXIST' ) {
+        //     res.statusCode = 409;
+        //     res.end('File is already exist');
+        //   } 
+        // });
+
+        // fileOut.on('close', () =>{
+        //   res.statusCode = 201;
+        //   res.end('File created');
+        // });
+          
 
         break;
       default:
