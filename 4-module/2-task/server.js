1665fca7b9601d3a45e9f231d84b1eb1f2c840f4
Danil Kavaliou <dkavaliou.by@gmail.com>
2019-07-05 18:14:50 +0300

    4m2t - cleanup

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -12,85 +12,76 @@ server.on('request', (req, res) => {
   const filepath = path.join(__dirname, 'files', pathname);
   
 
-  try {
-    switch (req.method) {
-      case 'POST':
-        // console.log('pathname', pathname)
-        // console.log('filepath', filepath)
-        // console.log('dirname', path.dirname(pathname));
+  switch (req.method) {
+    case 'POST':
   
-        if( path.dirname(pathname) != '.') {
-          res.statusCode = 400;
-          res.end('Path with dirs are not supported');
-          return;
-        }
+      if( path.dirname(pathname) != '.') {
+        res.statusCode = 400;
+        res.end('Path with dirs are not supported');
+        return;
+      }
         
-        const fileOut = fs.createWriteStream(filepath, { flags: 'wx'});
-        const limitStream = new LimitSizeStream({limit: 1048576, encoding: 'utf-8'});
+      const fileOut = fs.createWriteStream(filepath, { flags: 'wx'});
+      const limitStream = new LimitSizeStream({limit: 1048576, encoding: 'utf-8'});
 
 
-        req
-          .on('aborted', () => {
-            fs.unlink(filepath);
-            res.statusCode = 501;
-            res.end('Connection was lost');
-          })
-          .pipe(limitStream)
-          .on('error', (err) =>{
-            if (err && err.code == 'LIMIT_EXCEEDED'){
-              res.statusCode = 413;
-              res.end('File is too big'); 
-              fs.unlink(filepath);
-            }
-          })
-          .pipe(fileOut)
-          .on('error', (err) => {
-            if(err && err.code == 'EEXIST' ) {
-              res.statusCode = 409;
-              res.end('File is already exist');
-            } 
-          })
-          .on('close', () =>{
-            res.statusCode = 201;
-            res.end('File created');
-          });
-        
-        // req.pipe(limitStream).pipe(fileOut);
-        // req.on('aborted', () => {
+        // req
+        //   .on('aborted', () => {
         //     fs.unlink(filepath);
         //     res.statusCode = 501;
         //     res.end('Connection was lost');
+        //   })
+        //   .pipe(limitStream)
+        //   .on('error', (err) =>{
+        //     if (err && err.code == 'LIMIT_EXCEEDED'){
+        //       res.statusCode = 413;
+        //       res.end('File is too big'); 
+        //       fs.unlink(filepath);
+        //     }
+        //   })
+        //   .pipe(fileOut)
+        //   .on('error', (err) => {
+        //     if(err && err.code == 'EEXIST' ) {
+        //       res.statusCode = 409;
+        //       res.end('File is already exist');
+        //     } 
+        //   })
+        //   .on('close', () =>{
+        //     res.statusCode = 201;
+        //     res.end('File created');
         //   });
+        
+      req.pipe(limitStream).pipe(fileOut);
+      req.on('aborted', () => {
+          fs.unlink(filepath);
+          res.statusCode = 501;
+          res.end('Connection was lost');
+        });
 
-        // limitStream.on('error', (err) =>{
-        //   if (err && err.code == 'LIMIT_EXCEEDED'){
-        //     res.statusCode = 413;
-        //     res.end('File is too big'); 
-        //     fs.unlink(filepath);
-        //   }
-        // });
+      limitStream.on('error', (err) =>{
+        if (err && err.code == 'LIMIT_EXCEEDED'){
+          res.statusCode = 413;
+          res.end('File is too big'); 
+          fs.unlink(filepath);
+        }
+      });
 
-        // fileOut.on('error', (err) => {
-        //   if(err && err.code == 'EEXIST' ) {
-        //     res.statusCode = 409;
-        //     res.end('File is already exist');
-        //   } 
-        // });
+      fileOut.on('error', (err) => {
+        if(err && err.code == 'EEXIST' ) {
+          res.statusCode = 409;
+          res.end('File is already exist');
+        } 
+      });
 
-        // fileOut.on('close', () =>{
-        //   res.statusCode = 201;
-        //   res.end('File created');
-        // });
-          
+      fileOut.on('close', () =>{
+        res.statusCode = 201;
+        res.end('File created');
+      });
 
-        break;
-      default:
-        res.statusCode = 501;
-        res.end('Not implemented');
-    }
-  } catch (error) {
-    res.statusCode = 500;
-    res.end(`Unknown error ${error}`); 
+      break;
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
   }
 });
 
