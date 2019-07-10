2a71e19c14ba5e811639aaaf1756dad7219292b5
Danil Kavaliou <dkavaliou.by@gmail.com>
2019-07-10 12:05:40 +0300

    5m1t - koa chat

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,44 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const connections = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+
+  const message = await new Promise((resolve) => {
+    connections.add(resolve);
+
+    ctx.res.on('close', () => {
+      if( !ctx.res.finished ) {
+        connections.delete(resolve);
+        resolve();
+      }
+    });
+  });
+
+  ctx.response.body = message;
+  ctx.response.status = 200;
+  ctx.response.message = 'ok';
 });
 
 router.post('/publish', async (ctx, next) => {
+
+  let newMessage = ctx.request.body.message;
+
+  // console.log('ctx request', ctx.request.body.message);
+  // console.log('ctx newMessage', newMessage);
+  // console.log('connections', connections);
+  if( !newMessage ) {
+    ctx.throw(500, 'Message should not be empty');
+  }
+
+  for (resolve of connections ) {
+    resolve(newMessage);
+  }
+
+  connections.clear();
+  ctx.response.status = 200;
+  ctx.response.message = 'ok';
 });
 
 app.use(router.routes());
