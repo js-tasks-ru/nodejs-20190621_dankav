4f8a7ae463a9cf26e93ef2c3c39430c788d0dcd2
Danil Kavaliou <dkavaliou.by@gmail.com>
2019-08-06 18:05:21 +0300

    8m1t - initial version

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -3,6 +3,23 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const token = uuid();
+  console.log('body',  ctx.request.body )
+  const userData = {
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    verificationToken: token,
+    password: ctx.request.body.password,
+  };
+
+  const user = new User(userData);
+  await user.setPassword(userData.password);
+  await user.save();
+  
+  ctx.status = 200;
+  ctx.body = {status: 'ok'};
+  
+  return next();
 };
 
 module.exports.confirm = async (ctx, next) => {
