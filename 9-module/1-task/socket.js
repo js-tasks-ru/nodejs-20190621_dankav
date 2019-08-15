4ca0c2126b29c025a11537f1ab9f6d65eefe341b
Danil Kavaliou <dkavaliou.by@gmail.com>
2019-08-15 17:09:30 +0300

    9m1t - solution9m1t - solution9m1t - solution9m1t - solution9m1t - solution9m1t - solution9m1t - solution9m1t - solution9m1t - solution

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -7,11 +7,36 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const query = socket.handshake.query;
+    
+    if (!query.token) {
+      next(new Error('anonymous sessions are not allowed'));
+    }
+
+    const session = await Session.findOne({token: query.token}).populate('user');
+    if (!session) {
+      next(new Error('anonymous sessions are not allowed'));
+    }
+    socket.user = session.user;
+    // console.log('session', session);
+    // console.log('socket.user', socket.user);
+    // console.log('query', query);
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      console.log('socket.user', socket.user);
+      const message = new Message({
+        date: new Date(),
+        user: socket.user.displayName,
+        text: msg,
+        chat: socket.user.id,
+      });
+
+      console.log(message);
+      await message.save();
+    });
   });
 
   return io;
